6cabc32b0396327b89912716712167fa525321fe
SteliosPhanartzis <steliftw@gmail.com>
2018-12-21 09:38:53 -0500

    added containers

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,25 @@
 var leftH1 = document.getElementById('lefth1');
 var rightH1 = document.getElementById('righth1');
 
-function resolveAfter2Seconds() {
-    setTimeout(() => {
+function welcomeSplit() {
 	    leftH1.style.marginTop = "-100px";
 	    rightH1.style.marginTop = "+100px";
 	    leftH1.style.opacity = 0;
 	    rightH1.style.opacity = 0;
-	    setTimeout1(() => {
-		    rightH1.style.display = 'none';
-		    leftH1.style.display = 'none';
-	    }, 1000);
-    }, 1000);
 }
-resolveAfter2Seconds();
+
+function elementOffset(){
+	var width = window.innerWidth;
+	var nav = document.getElementById('pageNav');
+	console.log(width + " " + $("#pageNav").width() + " " + $("#projects").width());
+	nav.style.paddingLeft = (width*.99-$("#pageNav").width())/2 + "px";
+}
+
+function pageLoad(){
+welcomeSplit();
+elementOffset();
+}
+
+$(window).resize(function(){
+	elementOffset();
+});
