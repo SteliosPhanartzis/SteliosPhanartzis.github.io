3711bb2a0b09fa3e92edc1f7178302a175f21397
SteliosPhanartzis <stelios.phan@gmail.com>
2020-02-09 00:05:49 -0500

    Added selector/index at bottom

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-var leftH1 = document.getElementById('lefth1');
-var rightH1 = document.getElementById('righth1');
-
-function welcomeSplit() {
-	    leftH1.style.marginTop = "-100px";
-	    rightH1.style.marginTop = "+100px";
-	    leftH1.style.opacity = 0;
-	    rightH1.style.opacity = 0;
+const selector = document.querySelectorAll('.selector div');
+function shiftSelector() {
+	var project_index = 0;
+	getComputedStyle(document.querySelector('.carousel')).getPropertyValue('transform');
+	//Uses left offset dived by inner window width to get index
+	project_index = parseInt((parseInt(getComputedStyle(document.querySelector('.carousel')).getPropertyValue('left'))/parseInt(window.innerWidth) + 0.1)/(-0.6));
+	//Remove highlight from previous selector
+	if(project_index == selector.length)
+		project_index = 0;
+	selector[(project_index+selector.length-1)%selector.length].classList = "";
+	//Add highlight to next selector
+	selector[project_index].classList = "highlight";
+	setTimeout("shiftSelector()",1000)
 }
-
-function elementOffset(){
-	var width = window.innerWidth;
-	var nav = document.getElementById('pageNav');
-	console.log(width + " " + $("#pageNav").width() + " " + $("#projects").width());
-//	nav.style.paddingLeft = ((width)-$("#pageNav").width())/2 + "px";
-}
-
-jQuery.expr.filters.offscreen = function(el) {
-  var rect = el.getBoundingClientRect();
-  return (
-           (rect.x + rect.width) < 0 
-             || (rect.y + rect.height) < 0
-             || (rect.x > window.innerWidth || rect.y > window.innerHeight)
-         );
-};
-
-function pageLoad(){
-welcomeSplit();
-elementOffset();
-}
-
-$(window).resize(function(){
-	elementOffset();
-});
-
-$(window).scroll(function(){
-console.log($(':offscreen'));
-	
-});
+document.onload = shiftSelector();
