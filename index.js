1e1df1e6736c3e6c522f4b02df0387f979e790a3
SteliosPhanartzis <stelios.phan@gmail.com>
2020-02-09 17:21:14 -0500

    Update styling for better responsive design

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ function shiftSelector() {
 	var project_index = 0;
 	getComputedStyle(document.querySelector('.carousel')).getPropertyValue('transform');
 	//Uses left offset dived by inner window width to get index
-	project_index = parseInt((parseInt(getComputedStyle(document.querySelector('.carousel')).getPropertyValue('left'))/parseInt(window.innerWidth) + 0.1)/(-0.6));
+	let vw_ratio = (-1*parseInt(getComputedStyle(document.querySelector('.carousel-container')).getPropertyValue('width'))/parseInt(window.innerWidth));
+	let carousel_offset = (parseInt(getComputedStyle(document.querySelector('.carousel')).getPropertyValue('left'))/parseInt(window.innerWidth) + 0.1);
+	project_index = parseInt(carousel_offset/vw_ratio);
 	//Remove highlight from previous selector
 	if(project_index == selector.length)
 		project_index = 0;
