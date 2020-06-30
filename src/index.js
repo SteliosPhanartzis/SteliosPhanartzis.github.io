6e983aefe35304a2d20f24ea39deae5c87c03aec
SteliosPhanartzis <stelios.phan@gmail.com>
2020-06-29 23:22:25 -0400

    preloading images

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
-/* Snap to section */
 let sections = gsap.utils.toArray("section");
+var isScroll = true;
 var mobile = (navigator.userAgent.match(/Android/i)
             || navigator.userAgent.match(/webOS/i)
             || navigator.userAgent.match(/iPhone/i)
             || navigator.userAgent.match(/iPad/i)
             || navigator.userAgent.match(/iPod/i)
             || navigator.userAgent.match(/BlackBerry/i)
-            || navigator.userAgent.match(/Windows Phone/i))
-if(!mobile){
-    function goToSection(i, anim) {
-      gsap.to(window, {
+            || navigator.userAgent.match(/Windows Phone/i));
+/* Snap to section */
+function goToSection(i, anim) {
+    if(isScroll){
+        gsap.to(window, {
         scrollTo: {y: i*innerHeight, autoKill: false},
         duration: 1
-      });
-      if(anim)
-        anim.restart();
+        });
+        if(anim)
+            anim.restart();
     }
+}
+if (!mobile){
     sections.forEach((section, i) => {
       ScrollTrigger.create({
         trigger: section,
@@ -24,7 +27,8 @@ if(!mobile){
       });
       ScrollTrigger.create({
         trigger: section,
-        start: "bottom bottom",
+        scrub: true,
+        start: "top top",
         onEnterBack: () => goToSection(i),
       });
     });
@@ -33,44 +37,59 @@ if(!mobile){
 gsap.from(".title-left", {
     scrollTrigger: {
         trigger: ".title-left",
-        toggleActions: "restart reverse play pause"
+        toggleActions: "play none none none"
     },
     x: 20,
-    opacity: 0,
+    autoAlpha: 0,
     duration: 2,
-    delay: 1,
     ease: 'power1.inOut'
 });
 gsap.from(".title-right", {
     scrollTrigger: {
         trigger: ".title-right",
-        toggleActions: "restart none restart pause"
+        toggleActions: "play none none none"
     },
     x: -20,
-    opacity: 0,
+    autoAlpha: 0,
     duration: 2,
+    delay: 1,
     ease: 'power1.inOut'
 });
-/* Left and Right aligns */
-gsap.from(".left-align", {
+/* Bio in about me */
+gsap.from(".bio", {
     scrollTrigger: {
-        trigger: ".left-align",
-        toggleActions: "restart none restart reverse"
+        trigger: ".bio",
+        toggleActions: "restart reset restart reset"
     },
     x: -80,
-    opacity: 0,
+    autoAlpha: 0,
     duration: 1.5,
+    delay: 2,
     stagger: 0.5,
     ease: 'power2.out'
 });
+/* Work pictures */
+gsap.from(".work-items", {
+    scrollTrigger: {
+        trigger: ".work-items",
+        toggleActions: "restart reset restart reset"
+    },
+    x: -80,
+    autoAlpha: 0,
+    duration: 1.5,
+    delay: 2,
+    ease: 'power2.out'
+});
+/* Right aligns */
 gsap.from(".right-align", {
     scrollTrigger: {
         trigger: ".right-align",
-        toggleActions: "restart none restart pause"
+        toggleActions: "restart reset restart reset"
     },
     x: 80,
-    opacity: 0,
+    autoAlpha: 0,
     duration: 1.5,
+    delay: 1,
     stagger: 0.5,
     ease: 'power2.out'
 });
@@ -78,10 +97,33 @@ gsap.from(".right-align", {
 gsap.from(".full-pic", {
     scrollTrigger: {
         trigger: ".full-pic",
-        toggleActions: "restart none restart pause"
+        toggleActions: "restart reset restart reset"
     },
     x: 100,
-    opacity: 0,
+    autoAlpha: 0,
     duration: 2,
+    delay: 1,
     ease: 'power2.out'
+});
+// Determine whether section is scrolled to or jumped to
+window.addEventListener('wheel', function(e) {
+    isScroll = true;
+});
+document.onkeydown = (e) => {
+    switch (event.keyCode) {
+        case 33:
+        case 34:
+        case 37:
+        case 38:
+        case 39:
+        case 40:
+            isScroll = true;
+            break
+    }
+}
+var navItems = gsap.utils.toArray('.navItem');
+navItems.forEach((item) => {
+    addEventListener('click', function(e) {
+        isScroll = false;
+    });
 });
